08c1239ba111c38412a411d22b1023794871c71a
Евгений <Linless@Linless.local>
2015-08-24 14:01:06 +0500

    edit tableModel

--- App/table/tableModel.js
+++ App/table/tableModel.js
@@ -3,6 +3,12 @@
 
   function tableViewModel() {
     var self = this;
+    var names = ["Tashya V. Fuentes", "Asher X. Pennington", "Ariel H. Schmidt", "Bradley R. Hancock", "Simon V. Brewer"];
+
+    function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
 
     // Data
     self.items = ko.observableArray([
@@ -33,7 +39,7 @@
       self.items.remove(item);
     }
     self.addItem = function() {
-      self.items.push({"id":6, "name": "Simon V. Brewer111"});
+      self.items.push({"id":(self.items().length + 1), "name": names[getRandomInt(0, names.length - 1)]});
     }
     self.removeAll = function() {
       self.items.removeAll();
